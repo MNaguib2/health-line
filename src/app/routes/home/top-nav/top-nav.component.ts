7932ed058925f6cc70b41fd186c19934ff65a1d1
mena afefe <menaafefefawze@gmail.com>
2024-09-03 01:34:19 +0300

    start in top nav

---
+++ src/app/routes/home/top-nav/top-nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'health-line-top-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-nav.component.html',
+  styleUrl: './top-nav.component.scss'
+})
+export class TopNavComponent {
+
+}
