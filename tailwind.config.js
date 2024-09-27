c8b65fc79d57595b31426ced93b69124ce26d18a
Mena Afefe <menaafefefawze@gmail.com>
2024-09-27 14:03:38 +0300

    use grid tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   theme: {
     extend: {
       screens: {
+        '450px': '450px',
+        '900px': '900px',
+        '1050px': '1050px',
         '1200px': '1200px',
         '1280px': '1280px',
         '1400px': '1400px',
