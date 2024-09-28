40d4cd83fddbbac6fa9d5420f8ed6b531bfda901
Mena Afefe <menaafefefawze@gmail.com>
2024-09-28 03:32:38 +0300

    about clinic

---
+++ src/app/routes/home/about-clinic/about-clinic.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'health-line-about-clinic',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-clinic.component.html',
+  styleUrl: './about-clinic.component.scss',
+  animations: []
+})
+export class AboutClinicComponent {
+
+}
--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TopNavComponent } from "./top-nav/top-nav.component";
 import { PannerSlideComponent } from "./panner-slide/panner-slide.component";
+import { AboutClinicComponent } from "./about-clinic/about-clinic.component";
 
 @Component({
   selector: 'health-line-home',
   standalone: true,
-  imports: [TopNavComponent, PannerSlideComponent],
+  imports: [TopNavComponent, PannerSlideComponent, AboutClinicComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
