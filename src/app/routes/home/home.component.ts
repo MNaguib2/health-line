7e20abe5fd09831bb076b537b50612d1dfbcd818
Mena Afefe <menaafefefawze@gmail.com>
2024-10-12 17:41:58 +0300

    feedback

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -3,11 +3,12 @@ import { TopNavComponent } from "./top-nav/top-nav.component";
 import { PannerSlideComponent } from "./panner-slide/panner-slide.component";
 import { AboutClinicComponent } from "./about-clinic/about-clinic.component";
 import { DocumentationComponent } from "./documentation/documentation.component";
+import { FeedBackComponent } from "./feed-back/feed-back.component";
 
 @Component({
   selector: 'health-line-home',
   standalone: true,
-  imports: [TopNavComponent, PannerSlideComponent, AboutClinicComponent, DocumentationComponent],
+  imports: [TopNavComponent, PannerSlideComponent, AboutClinicComponent, DocumentationComponent, FeedBackComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
