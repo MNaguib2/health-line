546265324a44d9f112bc22b85de958fe3228a4f6
Mena Afefe <menaafefefawze@gmail.com>
2024-12-10 21:23:25 +0200

    ....

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -4,11 +4,12 @@ import { PannerSlideComponent } from "./panner-slide/panner-slide.component";
 import { AboutClinicComponent } from "./about-clinic/about-clinic.component";
 import { DocumentationComponent } from "./documentation/documentation.component";
 import { FeedBackComponent } from "./feed-back/feed-back.component";
+import { WhyChooseComponent } from "./why-choose/why-choose.component";
 
 @Component({
   selector: 'health-line-home',
   standalone: true,
-  imports: [TopNavComponent, PannerSlideComponent, AboutClinicComponent, DocumentationComponent, FeedBackComponent],
+  imports: [TopNavComponent, PannerSlideComponent, AboutClinicComponent, DocumentationComponent, FeedBackComponent, WhyChooseComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/routes/home/why-choose/why-choose.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'health-line-why-choose',
+  standalone: true,
+  imports: [],
+  templateUrl: './why-choose.component.html',
+  styleUrl: './why-choose.component.scss'
+})
+export class WhyChooseComponent {
+
+}
